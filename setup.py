7a84c3e91ec996c501be95276a38a42f9b0d3bfa
Viacheslav Rodionov <bepcyc@gmail.com>
2022-11-04 14:43:09 +0100

    change dir structure

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='vast',
-    version='0.1.0',
-    install_requires=['requests', 'pandas', 'argh'],
-    url='https://github.com/vast-ai/vast-python',
-    license='MIT',
-    author='Vast.ai',
-    author_email='support@vast.ai',
-    description='Vast.ai CLI',
-    packages=find_packages(),
-    entry_points={
-      'console_scripts': [
-          'vast=vast.cli:vast',
-          'vast-ai=vast.cli:vast_ai'
-          ]
-      }
-)
+setup()
